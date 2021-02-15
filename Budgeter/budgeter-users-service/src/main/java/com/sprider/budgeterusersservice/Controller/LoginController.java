bbe686b62975db99d7d13e60247f8015a18f746a
Ishan <ir7000129@gmail.com>
2021-02-15 17:25:27 +1200

    update

--- Budgeter/budgeter-users-service/src/main/java/com/sprider/budgeterusersservice/Controller/LoginController.java
+++ Budgeter/budgeter-users-service/src/main/java/com/sprider/budgeterusersservice/Controller/LoginController.java
@@ -1,8 +1,18 @@
 package com.sprider.budgeterusersservice.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
     
+    @GetMapping("/")
+    public String getIndex (){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLogin(){
+        return "login";
+    }
 }
