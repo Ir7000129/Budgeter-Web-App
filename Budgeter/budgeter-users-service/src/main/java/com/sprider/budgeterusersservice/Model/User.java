170b46ff24e833e5f849887d5de96b028e4cab52
Ishan <ir7000129@gmail.com>
2021-02-17 19:37:27 +1200

    Update

--- Budgeter/budgeter-users-service/src/main/java/com/sprider/budgeterusersservice/Model/User.java
+++ Budgeter/budgeter-users-service/src/main/java/com/sprider/budgeterusersservice/Model/User.java
@@ -3,28 +3,29 @@ package com.sprider.budgeterusersservice.Model;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Document(collection = "users")
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter @Setter
 public class User {
     @Id
     private String id;
+
+    @Indexed(unique = true, direction = IndexDirection.DESCENDING, dropDups = true)
+    private String emailAddress;
+    @Indexed(unique = true, direction = IndexDirection.DESCENDING, dropDups = true)
     private String userName;
     private String firstName;
     private String lastName;
     private String password;
-
-    public User() {
-    }
-
-    public User(String userName, String password, String id) {
-        this.userName = userName;
-        this.password = password;
-        this.id = id;
-    }
 }
